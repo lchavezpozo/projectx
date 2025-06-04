fff6aef14c668314223e5bb5a5be58b5d4aebe4c
Luis Chavez <lchavezpozo@gmail.com>
2025-06-04 11:13:11 -0500

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  
+  // Excluir seed.ts del build
+  webpack: (config: any, { isServer }: any) => {
+    if (isServer) {
+      // Excluir archivos específicos del build
+      config.externals = config.externals || [];
+      config.externals.push({
+        './prisma/seed': 'commonjs ./prisma/seed',
+        './prisma/seed.ts': 'commonjs ./prisma/seed.ts',
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
